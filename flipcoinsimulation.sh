430e81a2d19fdbda04d55107db62a0d7af7b4a7c
Zeenat100 <siddzee100@gmail.com>
2020-10-13 03:13:22 -0700

    Add Multiple Flip result

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -1,11 +1,18 @@
 heads=1
 tails=2
+heads_count=0
+tails_count=0
 echo "Welcome to flip coin simulation"
-result=$(( $RANDOM % 2 +1))
-echo $result
-if [[ $result -eq $heads ]]
-then
-	echo "Heads is the winner"
-else
-	echo "Tails is the winner"
-fi
+echo "Let flip the coin 50 times"
+for (( i=1 ; i <= 50 ; i++ ))
+do
+	result=$(( $RANDOM % 2 +1))
+	if [[ $result -eq $heads ]]
+	then
+		heads_count=$(($heads_count + 1 ))
+	else
+		tails_count=$(($tails_count + 1 ))
+	fi
+done
+echo "$heads_count times head won"
+echo "$tails_count times tail won"
