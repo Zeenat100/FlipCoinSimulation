24b847d9e7a8c7eee8d4870f2f729c8ca5b28a56
Zeenat100 <siddzee100@gmail.com>
2020-10-13 03:02:52 -0700

    Add Single Flip result

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -1,1 +1,11 @@
+heads=1
+tails=2
 echo "Welcome to flip coin simulation"
+result=$(( $RANDOM % 2 +1))
+echo $result
+if [[ $result -eq $heads ]]
+then
+	echo "Heads is the winner"
+else
+	echo "Tails is the winner"
+fi
